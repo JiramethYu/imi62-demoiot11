b599053de5a608b74b0d222590ffe7f1c3c0980f
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-08-18 21:21:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,21 +25,21 @@ $user_data = array_slice($user_data, 0);
 // Traverse array and display user data
 
 ?>  
-<iframe src="https://thingspeak.com/channels/1458765/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" height="500" width="500" frameBorder="0" title="Iframe Example"></iframe>
+<iframe src="https://thingspeak.com/channels/1458765/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" height="250" width="500" frameBorder="0" title="Iframe Example"></iframe>
  <?php
    foreach ($user_data as $user) {
 	echo "Humidity: ".$user->field1; 
       echo " % ";
    }?> 
  <br>   
-<iframe src="https://thingspeak.com/channels/1458765/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" height="500" width="500" frameBorder="0" title="Iframe Example"></iframe>
+<iframe src="https://thingspeak.com/channels/1458765/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" height="250" width="500" frameBorder="0" title="Iframe Example"></iframe>
 <?php
    foreach ($user_data as $user) {
 	echo "Temperature: ".$user->field2; 
       echo " C ";
    }?> 
  <br>   
-<iframe src="https://thingspeak.com/channels/1458765/maps/channel_show" height="500" width="500" frameBorder="0" title="Iframe Example"></iframe>
+<iframe src="https://thingspeak.com/channels/1458765/maps/channel_show" height="250" width="500" frameBorder="0" title="Iframe Example"></iframe>
 <br>     
  
    
