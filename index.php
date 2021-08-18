e65fb73d124b8dff13cfa45c1327e6a24471735b
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-08-18 20:51:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,6 @@
 <html>
 <body>
    
-$response = file_get_contents("https://api.thingspeak.com/channels/1458765/feeds.json?results=2",false,stream_context_create($arrContextOptions));
-$json=json_decode($response )
-$name=$json->channel->name;  
-$humidity=$json->feeds[1]->field1;
-$temperature=$json->feeds[1]->field2;
-$time=$json->feeds[1]->created_total;
-$newtime= date("d-m-Y H:i", strtotime($time));
-
 <h1>62101456 Jirameth Yukachain</h1>
    
 <iframe src="https://thingspeak.com/channels/1458765/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" height="500" width="500" frameBorder="0" title="Iframe Example"></iframe>
