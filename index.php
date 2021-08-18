454b76d1021e6aa0af11a4252a3e1bb01770a419
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-08-18 15:38:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-$api_url = 'https://api.thingspeak.com/channels/1458765/feeds.json?results=2';
+<?php
+
+$api_url = ' https://api.thingspeak.com/channels/1458765/feeds.json?results=2';
+$json_data = file_get_contents($api_url);
+$response_data = json_decode($json_data);
+$user_data = $response_data->data;
+$user_data = array_slice($user_data, 0, 9);
+
+foreach ($user_data as $user) {
+	echo "name: ".$user->Temperature;
+	echo "<br />";
+	echo "name: ".$user->Huminity;
+	echo "<br /> <br />";
+}
+
+?>
