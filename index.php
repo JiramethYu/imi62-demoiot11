5d352c3adf4228e00e1d0584a6144e17b4043690
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-08-18 15:30:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
+<!DOCTYPE html>
+<html>
+<title>HTML Tutorial</title>
+<body>
+
 <h1 class="content">62101456 Jirameth Yukachain</h1>
-$json = $ch->get("https://api.thingspeak.com/channels/1458765/feeds.json?results=2" alt="Workplace" usemap="#workmap");
+
+</body>
+</html>
