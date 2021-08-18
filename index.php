57e5953d834fde976d7cb4ffd374d706dc66f66e
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-08-18 16:02:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,16 @@
  <br>
   
 <div class = "container">
+ <div class="row">
+  <div class = "col-sm-7">
  <h2>board</h2>
-
+ <div class = "container">
+ <div class="row">
+  <iframe class="col" src =" https://api.thingspeak.com/channels/1458765/feeds.json?results=2">
+  
+ </div>
+ 
+   
 
 </body>
 </html>
