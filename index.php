31a42d31fd5617711ae8b92ae7b1cf087999b434
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-08-18 17:49:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,14 @@
 
 
 <h1>62101456 Jirameth Yukachain</h1>
-<br>
 
-<div class ="container">
-    <div class="row">
-        <div class="col-sm-7">
-<h2>Display Board</h2>
-<div class ="container">
-    <div class="row">
+
+
     <iframe class="col" src="https://thingspeak.com/channels/1458765/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" title="Humidity"></iframe>
-    </div>
-</br>
-    <div class="row">
+   
+   
     <iframe class="col" src="https://thingspeak.com/channels/1458765/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" title="Temperature"></iframe>
-    </div>
-</br>    
-<div class="row">
-    <iframe class="col" src="https://thingspeak.com/channels/1458765/maps/channel_show" title="Location"></iframe>
-    </div>
-</br>    
+ 
 
-</div>
-     </div>
-     
+    <iframe class="col" src="https://thingspeak.com/channels/1458765/maps/channel_show" title="Location"></iframe>
+  
