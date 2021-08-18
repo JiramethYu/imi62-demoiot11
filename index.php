f05d26d6b47e644dbb5c5a54e40f737589b296b1
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-08-18 18:06:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 
 <h1>62101456 Jirameth Yukachain</h1>
 
-<iframe src="https://thingspeak.com/channels/1458765/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"style="border: 0" height="100%" width="100%"title="Iframe Example"></iframe>
+
    
+   <iframe src="https://thingspeak.com/channels/1458765/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="300" height="300" frameBorder="0"></iframe>
  </body>
 </html>   
