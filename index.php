b2d46fa2b69fcc9d0b4340cd78b3c2d741baddc3
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-08-18 15:49:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
-<?php
+<!DOCTYPE html>
+<html>
+<body>
 
-$api_url = ' https://api.thingspeak.com/channels/1458765/feeds.json?results=2';
-$json_data = file_get_contents($api_url);
-$response_data = json_decode($json_data);
-$user_data = $response_data->data;
-$user_data = array_slice($user_data, 0, 9);
+<h1>62101456 Jirameth Yukachain</h1>
 
-foreach ($user_data as $user) {
-	echo "name: ".$user->Temperature;
-	echo "<br />";
-	echo "name: ".$user->Huminity;
-	echo "<br /> <br />";
-}
 
-?>
+</body>
+</html>
