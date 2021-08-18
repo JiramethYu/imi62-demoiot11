20e694cb1677cb33de07829ec3edcce326783cd5
JiramethYu <71056765+JiramethYu@users.noreply.github.com>
2021-08-18 15:28:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
 <h1 class="content">62101456 Jirameth Yukachain</h1>
-<img src="https://api.thingspeak.com/channels/1458765/feeds.json?results=2" alt="Workplace" usemap="#workmap">
+$json = $ch->get("https://api.thingspeak.com/channels/1458765/feeds.json?results=2" alt="Workplace" usemap="#workmap");
